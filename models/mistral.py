c35f1d0031a11c29410b6d322c219a786eb6f473
Alex.Mills <alex@makerops.com>
2024-05-16 23:09:14 -0500

    fix pdf txt

--- models/mistral.py
+++ models/mistral.py
@@ -39,7 +39,8 @@ def extract_text_from_pdf(pdf_path):
     return text
 
 pdf_path = "/Users/alex.mills/Downloads/sales.pdf"
-pdf_text = extract_text_from_pdf(pdf_path)
+# pdf_text = extract_text_from_pdf(pdf_path)
+pdf_text = "Alex sold 5 items in January. Dan sold 8 items in January. Dan also sold 9 items in February. Jeremy sold 6 items in March." # extract_text_from_pdf(pdf_path)
 
 csv_path = "/Users/alex.mills/Downloads/Jira.csv"
 csv_text = "" # extract_text_from_csv(csv_path)
